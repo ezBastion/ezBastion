fadc105e8eddd66a7600574510e3a7248806f390
jfbutkiewicz <jean-francois.butkiewicz@veonergie.com>
2022-02-22 01:24:02 +0100

    function iintrospect done, if jti not found in cache, check the user in AD

--- cmd/ezb_sta/ctrl/introspect.go
+++ cmd/ezb_sta/ctrl/introspect.go
@@ -14,30 +14,48 @@ func Introspect(ldapclient *models.Ldapinfo) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		j, err := c.Get("jti")
 		if err == false {
-			c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0003"))
+			c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0003, no jti sent"))
 		}
 		user, ok := ldapclient.JTIMap[fmt.Sprintf("%s", j)]
 		if ok {
-			username := strings.Split(user, "\\")
-			u := new(models.IntrospectUser)
-			ADobj, err := middleware.F_GetADproperties(username[1], ldapclient)
+			u, err := getUserFromAD(fmt.Sprintf("%s", user), ldapclient)
 			if err != nil {
-				c.AbortWithError(http.StatusConflict, errors.New("#STA-INSP0002"))
+				c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0002, no user found in AD"))
 			}
-			u.Groups = ADobj.Groups
-			u.Ou = ADobj.Ou
-			u.Samaccountname = ADobj.Samaccountname
-			u.Ntaccount = ADobj.Ntaccount
-			u.Givenname = ADobj.Givenname
-			u.Emailaddress = ADobj.Emailaddress
-			u.Description = ADobj.Description
-			u.Distinguishedname = ADobj.Distinguishedname
-			u.Displayname = ADobj.Displayname
-
 			c.JSON(http.StatusOK, u)
 		} else {
-			c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0001"))
+			// the jti is not in the cache map, lets compute it
+			userobj, ok := c.Get("user")
+			if ok == false {
+				c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0003, no user sent"))
+			}
+			username := fmt.Sprintf("%s", userobj)
+			u, err := getUserFromAD(username, ldapclient)
+			if err != nil {
+				c.AbortWithError(http.StatusNoContent, errors.New("#STA-INSP0002, no user found in AD"))
+			}
+			c.JSON(http.StatusOK, u)
+
 		}
 	}
+}
 
+func getUserFromAD(user string, ldapclient *models.Ldapinfo) (u *models.IntrospectUser, err error) {
+	username := strings.Split(user, "\\")
+	u = new(models.IntrospectUser)
+	ADobj, err := middleware.F_GetADproperties(username[1], ldapclient)
+	if err != nil {
+		return nil, errors.New("#STA-INSP0002")
+	}
+	u.Groups = ADobj.Groups
+	u.Ou = ADobj.Ou
+	u.Samaccountname = ADobj.Samaccountname
+	u.Ntaccount = ADobj.Ntaccount
+	u.Givenname = ADobj.Givenname
+	u.Emailaddress = ADobj.Emailaddress
+	u.Description = ADobj.Description
+	u.Distinguishedname = ADobj.Distinguishedname
+	u.Displayname = ADobj.Displayname
+
+	return u, nil
 }
